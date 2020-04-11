a11f0317ebcd1e7eeb2359d6f56904722c0ff22b
lyj <knightlyj@163.com>
2020-04-11 09:39:59 +0800

    remove build files

--- firmware/build/sys_time.d
+++
@@ -1,6 +0,0 @@
-build/sys_time.o: common/mcu-misc/independent/src/sys_time.c \
- common/mcu-misc/independent/inc/sys_time.h common/clib/inc/cl_common.h
-
-common/mcu-misc/independent/inc/sys_time.h:
-
-common/clib/inc/cl_common.h:
